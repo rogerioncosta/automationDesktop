f1e722bfb5a3b9ed5d13cba158e7782d699adb9b
Rogério Nunes Costa <60291930+rogerioncosta@users.noreply.github.com>
2023-09-27 00:32:07 -0300

    Add files via upload

---
+++ ScriptsTestesDesktop/Telas/NovoProdutoTela.js
@@ -0,0 +1,11 @@
+﻿function submeterNovoProdutoComErro(nome, valor, cores) {
+  // Preenchendo os dados do produto
+  let formularioAdicaoProduto = Aliases.Lojinha.frmPrincipal.PageControl1.TabSheet3.Panel6;
+  formularioAdicaoProduto.edtNovoNomeProduto.SetText(nome);
+  formularioAdicaoProduto.edtNovoValorProduto.SetText(valor);
+  formularioAdicaoProduto.edtNovoCoresProduto.SetText(cores);
+  
+  // Pressionar o botão de adição de produto
+  formularioAdicaoProduto.btnSalvarProduto.ClickButton();
+}
+module.exports.submeterNovoProdutoComErro = submeterNovoProdutoComErro;
